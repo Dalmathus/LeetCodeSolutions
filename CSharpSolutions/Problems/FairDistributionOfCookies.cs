49e68ea28bbabda1fabba1fa42e434110e46b080
James Luxton <jameswluxton@gmail.com>
2023-07-05 18:07:49 +1200

    Test case setup, start of backtracking imp

---
+++ CSharpSolutions/Problems/FairDistributionOfCookies.cs
@@ -0,0 +1,51 @@
+﻿using CSharpSolutions.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions.Problems
+{
+    /// <summary>
+    /// You are given an integer array cookies, where cookies[i] denotes the number of cookies in the ith bag. You are also given an integer k that denotes the number of children to distribute all the bags of cookies to. All the cookies in the same bag must go to the same child and cannot be split up.
+    /// The unfairness of a distribution is defined as the maximum total cookies obtained by a single child in the distribution.
+    /// Return the minimum unfairness of all distributions.
+    /// 
+    /// Constraints:
+    /// 2 <= cookies.length <= 8
+    /// 1 <= cookies[i] <= 105
+    /// 2 <= k <= cookies.length
+    /// </summary>
+    public class FairDistributionOfCookies : Solution
+    {
+        public static int DistributeCookies(int[] cookies, int k)
+        {
+
+            // In order to complete this a back tracking algorithm should be used to get the set of sets from cookies
+
+            List<List<int>> subsets = new List<List<int>>();
+            List<int> input = cookies.ToList();
+
+            FindSubsets(subsets, input, new List<int>(), 0);
+
+            return 0;
+
+        }
+
+        public static void FindSubsets(List<List<int>> subsets, List<int> nums, List<int> output, int index) {
+            
+            if (index == nums.Count)
+            {
+                subsets.Add(output);
+                return;
+            }
+
+            // This call if proceeding down the tree without the current index value
+            FindSubsets(subsets, nums, new List<int>(output), index + 1);
+            output.Add(nums[index]);
+            // This call is proceeding down the tree with the current index value
+            FindSubsets(subsets, nums, new List<int>(output), index + 1);
+        }
+    }
+}
---
+++ LeetCodeTests/FairDistributionOfCookiesTests.cs
@@ -0,0 +1,46 @@
+﻿using CSharpSolutions.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTests
+{
+    [TestClass]
+    public class FairDistributionOfCookiesTests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            int[] cookies = new int[] { 8, 15, 10, 20, 8 };
+            int k = 2;
+
+            int result = FairDistributionOfCookies.DistributeCookies(cookies, k);
+
+            Assert.AreEqual(31, result);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            int[] cookies = new int[] { 6, 1, 3, 2, 2, 4, 1, 2 };
+            int k = 3;
+
+            int result = FairDistributionOfCookies.DistributeCookies(cookies, k);
+
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            int[] cookies = new int[] { 1, 2, 3 };
+            int k = 3;
+
+            int result = FairDistributionOfCookies.DistributeCookies(cookies, k);
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
