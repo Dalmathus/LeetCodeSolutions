319013e31644f4ba6a7b7f50647efc063fae9494
James Luxton <jameswluxton@gmail.com>
2023-07-06 23:50:08 +1200

    A verrrrry slow solution I was using to practice backtracking

---
+++ CSharpSolutions/Problems/MinimumSizeSubarraySum.cs
@@ -0,0 +1,87 @@
+﻿using CSharpSolutions.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CSharpSolutions.Problems
+{
+    public class MinimumSizeSubarraySum : Solution
+    {
+        public static int MinSubArrayLen(int target, int[] nums)
+        {
+            List<List<int>> subset = new List<List<int>>();
+
+            if (nums.Sum() < target) return 0;
+
+            BuildSubset(subset, target, new List<int>(), nums.ToList(), 0);
+
+            DeleteInvalidSubarrays(subset, nums);
+
+            return subset.Min(x => x.Count);
+        }
+
+        public static void BuildSubset(List<List<int>> subset, int target, List<int> output, List<int> nums, int index)
+        {
+            if (subset.Count > 0 && subset.Min(x => x.Count) < output.Count)
+            {
+                return;
+            }
+
+            if (output.Sum() >= target)
+            {
+                subset.Add(new List<int>(output));
+                return;
+            }
+
+            if (index == nums.Count)
+            {
+                return;
+            }
+
+            BuildSubset(subset, target, new List<int>(output), nums, index + 1);
+
+            output.Add(nums[index]);
+            BuildSubset(subset, target, new List<int>(output), nums, index + 1);
+        }
+
+        public static void DeleteInvalidSubarrays(List<List<int>> subset, int[] nums)
+        {
+            List<int> masterArray = new List<int>(nums);
+
+            int n = masterArray.Count;
+
+            for (int listNum = subset.Count - 1; listNum >= 0; listNum--)
+            {
+                int m = subset[listNum].Count;
+                bool keepSubset = false;
+                int i = 0, j = 0;
+
+                while (i < n && j < m)
+                {
+                    if (masterArray[i] == subset[listNum][j])
+                    {
+                        i++;
+                        j++;
+
+                        if (j == m)
+                            keepSubset = true;
+                    }
+                    else
+                    {
+                        i = i - j + 1;
+                        j = 0;
+                    }
+                }
+
+                if (!keepSubset)
+                {
+                    subset.RemoveAt(listNum);
+                }
+            }
+        }
+    }
+}
---
+++ LeetCodeTests/MinimumSizeSubarraySumTests.cs
@@ -0,0 +1,68 @@
+﻿using CSharpSolutions.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTests
+{
+    [TestClass]
+    public class MinimumSizeSubarraySumTests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            int[] nums = new int[] { 2, 3, 1, 2, 4, 3 };
+            int target = 7;
+
+            int result = MinimumSizeSubarraySum.MinSubArrayLen(target, nums);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            int[] nums = new int[] { 1, 4, 4 };
+            int target = 4;
+
+            int result = MinimumSizeSubarraySum.MinSubArrayLen(target, nums);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            int[] nums = new int[] { 1, 1, 1, 1, 1, 1, 1, 1 };
+            int target = 11;
+
+            int result = MinimumSizeSubarraySum.MinSubArrayLen(target, nums);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 5 };
+            int target = 15;
+
+            int result = MinimumSizeSubarraySum.MinSubArrayLen(target, nums);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Example5()
+        {
+            int[] nums = new int[] { 12, 28, 83, 4, 25, 26, 25, 2, 25, 25, 25, 12 };
+            int target = 213;
+
+            int result = MinimumSizeSubarraySum.MinSubArrayLen(target, nums);
+
+            Assert.AreEqual(8, result);
+        }
+    }
+}
