6e6bb44b8ae6bae4a364db78cf5fcd12265055a4
James Luxton <jameswluxton@gmail.com>
2023-07-09 20:56:12 +1200

    Problem Solution

---
+++ CSharpSolutions/Problems/Sort/HeightCheckers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions.Problems.Sort
+{
+    public class HeightCheckers
+    {
+        public int HeightChecker(int[] heights)
+        {
+            List<int> sortedHeights = new List<int>(heights);
+            sortedHeights.Sort();
+            int mismatchingIndexes = 0;
+
+            for (int i = 0; i < sortedHeights.Count; i++)
+            {
+                if (heights[i] != sortedHeights[i]) mismatchingIndexes++;
+            }
+            
+            return mismatchingIndexes;
+        }
+    }
+}
---
+++ LeetCodeTests/Sort/HeightCheckerTests.cs
@@ -0,0 +1,22 @@
+﻿using CSharpSolutions.Problems.Sort;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTests.Sort
+{
+    [TestClass]
+    public class HeightCheckerTests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            HeightCheckers hs = new HeightCheckers();
+            int[] heights = { 1, 1, 4, 2, 1, 3 };
+            int results = hs.HeightChecker(heights);
+            Assert.AreEqual(3, results);
+        }
+    }
+}
