5523dc2ecad846f2fb647b441ce60ed7ba066ecd
James Luxton <jameswluxton@gmail.com>
2023-07-09 23:19:31 +1200

    https://leetcode.com/problems/dota2-senate/solutions/?envType=study-plan-v2&envId=leetcode-75

---
+++ CSharpSolutions/Problems/Queue/Dota2Senate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions.Problems.Queue
+{
+    public class Dota2Senate
+    {
+        public string PredictPartyVictory(string senate)
+        {
+            List<char> votes = senate.ToList();
+
+            int rDeny = 0;
+            int dDeny = 0;
+
+            while (true)
+            {   
+                for (int i = 0; i < votes.Count; i++) {
+                    if (votes[i] == 'R' && rDeny == 0)
+                    {
+                        dDeny++;
+                        continue;
+                    }
+
+                    if (votes[i] == 'R' && rDeny > 0)
+                    {
+                        votes.RemoveAt(i);
+                        i--;
+                        rDeny--;
+                        continue;
+                    }
+
+                    if (votes[i] == 'D' && dDeny == 0)
+                    {
+                        rDeny++;
+                        continue;
+                    }
+
+                    if (votes[i] == 'D' && dDeny > 0)
+                    {
+                        votes.RemoveAt(i);
+                        i--;
+                        dDeny--;
+                        continue;
+                    }
+                }
+
+                if (!votes.Contains('R')) return "Dire";
+                if (!votes.Contains('D')) return "Radiant";
+            }
+        }
+    }
+}
---
+++ LeetCodeTests/Queue/Dota2SenateTests.cs
@@ -0,0 +1,40 @@
+﻿using CSharpSolutions.Problems.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTests.NewFolder
+{
+    [TestClass]
+    public class Dota2SenateTests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            Dota2Senate ds = new Dota2Senate();
+            string result = ds.PredictPartyVictory("RD");
+
+            Assert.AreEqual("Radiant", result);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            Dota2Senate ds = new Dota2Senate();
+            string result = ds.PredictPartyVictory("RDD");
+
+            Assert.AreEqual("Dire", result);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            Dota2Senate ds = new Dota2Senate();
+            string result = ds.PredictPartyVictory("RRR");
+
+            Assert.AreEqual("Radiant", result);
+        }
+    }
+}
